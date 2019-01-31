1980fa57ea6a1cd1a08e75599c1bce46fe1f8ce1
CM Student <cmstudent@UHWO-ACM-C-07.local>
2019-01-31 10:24:39 -1000

    Completed Swift Playgrounds Two

---
+++ SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var goodGrade = "I've been doing all of my work"
+if goodGrade == "I've been doing all of my work"
+{
+    print("I should be getting an A at this point")
+}
+else
+{
+    print("I need to work harder in this class")
+}
+var gradeA = true
+if gradeA {
+    print("I really like this class")
+}
+else
+{
+    print("I'm not so cure about coding")
+}
+
